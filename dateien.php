35ee9ffba80b3937048f54d114f02ba91024464b
Gillbador <zavel.illia@web.de>
2016-12-04 02:33:37 +0100

    Als Kontrolle für Session und Abmelden.

---
+++ dateien.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+
+    <title>Dateien</title>
+    <meta charset="UTF-8">
+    <?php include "session.php"; ?>
+
+</head>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Illia
+ * Date: 04.12.2016
+ * Time: 02:01
+ */
+
+
+
+echo "Dateien<br/><br />";
+
+
+echo "Hier geht es zur <br /><a href=\"abmelden.php\"> Abmeldung!</a><br />";
+
+?>
