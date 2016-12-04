c6ef71bdda0bb150bfa190e3a12b09e053fc2349
Gillbador <zavel.illia@web.de>
2016-12-04 02:34:22 +0100

    Neu

--- abmelden.php
+++ abmelden.php
@@ -9,7 +9,7 @@ session_start();
 include "connection.php";
 
 session_destroy();
-echo ("Bis zum nächsten Mal! <br> <a href=https://mars.iuk.hdm-stuttgart.de/~nl035/index.php>Zur Startseite</a>");
+echo ("Bis zum nächsten Mal! <br> <a href=login-form.html>Zur Startseite</a>");
 ?>
 </body>
 
