4e146f3a5b47d540d016ad95860991d98ca2f846
IlliaZ <zavel.illia@web.de>
2017-02-07 01:23:29 +0100

    Mit individuellem Namen

--- uploadjoin.php
+++ uploadjoin.php
@@ -9,14 +9,14 @@ session_start();
 include "conn.php";
 
 $login = $_SESSION['loginname'];
-$filename = $_SESSION['filename'];
+// $filename = $_SESSION['filename'];
 
 echo "Username: $login     " ;
 
 if(isset($_POST['submit'])) {
 
     $login = $_SESSION['loginname'];
-    $filename = $_SESSION['filename'];
+   // $filename = $_SESSION['filename'];
 
     $datei = $_FILES['bilddatei']['name'];
     $tmp_datei = $_FILES['bilddatei']['tmp_name'];
@@ -29,6 +29,11 @@ if(isset($_POST['submit'])) {
     $ordnerd = ($ordner . $datei);
     $dateiform = pathinfo($ordner_datei, PATHINFO_EXTENSION);
 
+    $indivdiual = $login.'_';
+
+    $nurname = $indivdiual . $datei;
+
+    $gesamt = $ordner .  $indivdiual . $datei;
 
 
     if (empty($datei)) {
@@ -69,10 +74,10 @@ if(isset($_POST['submit'])) {
 
 
 
-    if (move_uploaded_file($tmp_datei, $ordner_datei)) {
-            echo 'Coolio: <a href="' . $ordner_datei . '">' . $ordner_datei . '</a>';
-            $sql = $db->prepare("INSERT INTO files (name, typ, size, username) VALUES(:ordner_datei,:typ,:size,:login)");
-            $sql->bindParam(':ordner_datei', $ordner_datei, PDO::PARAM_STR);
+    if (move_uploaded_file($tmp_datei, $ordner . $indivdiual . $datei)) {
+            echo 'Coolio: <a href="' . $gesamt . '">' . $gesamt . '</a>';
+            $sql = $db->prepare("INSERT INTO files (name, typ, size, username) VALUES(:gesamt,:typ,:size,:login)");
+            $sql->bindParam(':gesamt', $nurname, PDO::PARAM_STR);
             $sql->bindParam(':typ', $typ, PDO::PARAM_STR);
             $sql->bindValue(':size', $size, PDO::PARAM_INT);
             $sql->bindValue(':login', $login, PDO::PARAM_STR);
